b16e8cd7e541caf303de4800185d8bab83ce8cc8
naquiyaa <n.asghar_18633@khi.iba.edu.pk>
2022-05-10 17:01:23 +0500

    - checking commit

--- lib/ingredient_scan_feature/ingredient_list.dart
+++ lib/ingredient_scan_feature/ingredient_list.dart
@@ -22,10 +22,156 @@ class IngredientsList extends StatefulWidget {
 }
 
 class _IngredientsListState extends State<IngredientsList> {
+  late List<Ingredient> ingredientList;
   final _formKey = GlobalKey<FormState>();
-  late TextEditingController productName_controller= TextEditingController();
+  late TextEditingController productName_controller = TextEditingController();
 
-  List<Ingredient> ingredientList = [];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarDetails(screenName: 'Ingredient List'),
+      // bottomNavigationBar: BottomBar(),
+      body: Container(
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: displayWidth(context) * 0.04),
+                child: TextFormField(
+                    controller: productName_controller,
+                    validator: (productName) {
+                      if (productName_controller.text.isEmpty) {
+                        print("validated");
+                        return "* Required";
+                      } else {
+                        return null;
+                      }
+                    },
+                    autofocus: false,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Enter Product Name',
+                      hintStyle: GoogleFonts.rambla(
+                          color: Color(0xff283618),
+                          fontSize: displayHeight(context) * 0.025,
+                          fontStyle: FontStyle.italic),
+                      fillColor: Color(0xffDADBC6),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide: BorderSide(
+                              width: displayWidth(context) * 0.03,
+                              style: BorderStyle.solid)),
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.all(displayHeight(context) * 0.01),
+                    )),
+              ),
+
+              ReemKufi_Green_Bold(
+                  textValue: 'Ingredients:',
+                  size: displayHeight(context) * 0.04),
+              IngredientListView(
+                  ingredientName: widget.ingredientName,
+                  ingredientList: (value) {
+                    ingredientList = value;
+                  }),
+              ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    Provider.of<ScannedProductProvider>(context, listen: false)
+                        .storeScannedProduct(ScannedProduct(
+                            productName: productName_controller.text,
+                            ingredientList: ingredientList));
+                  }
+                  // showDialog(
+                  //   barrierDismissible: false,
+                  //   context: context, // user must tap button!
+                  //   builder: (context) {
+                  //     return AlertDialog(
+                  //       backgroundColor: const Color(0xff283618),
+                  //       title: Column(
+                  //         children: [
+                  //           ReemKufiOffwhite(textValue: 'Added to Scanned Product', size: displayHeight(context) * 0.04),
+                  //         ],
+                  //       ),
+                  //       actions: <Widget>[
+                  //         Padding(
+                  //           padding: const EdgeInsets.all(8.0),
+                  //           child: Row(
+                  //             mainAxisAlignment:
+                  //             MainAxisAlignment.spaceEvenly,
+                  //             children: [
+                  //               TextButton(
+                  //                 style: TextButton.styleFrom(
+                  //                     backgroundColor: Color(0xffFFFDF4)),
+                  //                 child: Padding(
+                  //                   padding: const EdgeInsets.symmetric(
+                  //                       horizontal: 18.0),
+                  //                   child: Text('OK',
+                  //                       style: GoogleFonts.reemKufi(
+                  //                           color: Colors.black,
+                  //                           fontSize:
+                  //                           displayHeight(context) *
+                  //                               0.03)),
+                  //                 ),
+                  //                 onPressed: () {
+                  //                   Provider.of<ScannedProductProvider>(context, listen: false).storeScannedProduct(ScannedProduct(
+                  //                       productName: productName_controller.text,
+                  //                       ingredientList: ingredientList));
+                  //                   Navigator.pushAndRemoveUntil(
+                  //                     context,
+                  //                     MaterialPageRoute(
+                  //                       builder: (BuildContext context) => IngredientsList( ingredientName: widget.ingredientName),
+                  //                     ),
+                  //                         (route) => false,
+                  //                   );
+                  //                 },
+                  //               ),
+                  //             ],
+                  //           ),
+                  //         ),
+                  //       ],
+                  //     );
+                  //   },
+                  // );
+                },
+                style: ElevatedButton.styleFrom(
+                  side: BorderSide(width: displayWidth(context) * 0.2),
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  primary: Color(0xFF283618),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0),
+                  ),
+                ),
+                child: Text('Add to Scanned Products',
+                    style: GoogleFonts.reemKufi(
+                        color: Colors.white, fontSize: 20)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IngredientListView extends StatefulWidget {
+  final Function(List<Ingredient>) ingredientList;
+
+  final List<String> ingredientName;
+  const IngredientListView(
+      {Key? key, required this.ingredientName, required this.ingredientList})
+      : super(key: key);
+
+  @override
+  _IngredientListViewState createState() => _IngredientListViewState();
+}
+
+class _IngredientListViewState extends State<IngredientListView> {
+  late List<Ingredient> ingredientList;
 
   Future<List<Ingredient>> ExtractIngredientInfo(
       context, List<String> ingredientName) async {
@@ -46,176 +192,62 @@ class _IngredientsListState extends State<IngredientsList> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBarDetails(screenName: 'Ingredient List'),
-      // bottomNavigationBar: BottomBar(),
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            ReemKufi_Green_Bold(
-                textValue: 'Ingredients:', size: displayHeight(context) * 0.04),
-            FutureBuilder(
-                future: ExtractIngredientInfo(context, widget.ingredientName),
-                builder: (context, AsyncSnapshot<List<Ingredient>> snapshot) {
-                  if (!snapshot.hasData) {
-                    return Center(child: CircularProgressIndicator());
-                  } else {
-                    //print("here ak lle");
+    return FutureBuilder(
+        future: ExtractIngredientInfo(context, widget.ingredientName),
+        builder: (context, AsyncSnapshot<List<Ingredient>> snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            //print("here ak lle");
+            print(snapshot.data?.length);
+            return Expanded(
+              child: ListView.builder(
+                  itemCount: snapshot.data?.length,
+                  itemBuilder: (context, index) {
                     print(snapshot.data?.length);
-                    return Expanded(
-                      child: ListView.builder(
-                          itemCount: snapshot.data?.length,
-                          itemBuilder: (context, index) {
-                            print(snapshot.data?.length);
-                            return InkWell(
-                              onTap: () {
-                                Navigator.of(context).push(MaterialPageRoute(
-                                    builder: (context) => IngredientDetails(
-                                        ingredient: snapshot.data![index])));
-                              },
-                              child: Container(
-                                  alignment: Alignment.center,
-                                  margin: EdgeInsets.fromLTRB(8, 8, 8, 8),
-                                  width: double.infinity,
-                                  decoration: BoxDecoration(
-                                    color: const Color(0xFFDADBC6),
-                                    borderRadius: BorderRadius.circular(
-                                        20), //border corner radius
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Row(
-                                      mainAxisAlignment:
-                                          MainAxisAlignment.spaceBetween,
-                                      children: [
-                                        ReemKufi_Green_Italic(
-                                            textValue: snapshot
-                                                .data![index].ingredientName,
-                                            size:
-                                                displayHeight(context) * 0.02),
-                                        IconButton(
-                                            onPressed: () {
-                                              Navigator.push(
-                                                  context,
-                                                  MaterialPageRoute(
-                                                    builder: (context) =>
-                                                        IngredientDetails(
-                                                            ingredient: snapshot
-                                                                .data![index]),
-                                                  ));
-                                            },
-                                            icon: Icon(
-                                                Icons.arrow_forward_rounded)),
-                                      ],
-                                    ),
-                                  )),
-                            );
-                          }),
-                    );
-                  }
-                }),
-            ElevatedButton(
-              onPressed: () async {
-                return showDialog(
-                    barrierDismissible: false,
-                    context: context, // user must tap button!
-                    builder: (context) {
-                print("are u working");
-
-                 return  Form(
-                   key: _formKey,
-                   child: AlertDialog(
-                      backgroundColor: const Color(0xff283618),
-                      title: ReemKufiOffwhite(textValue: 'Enter product name',
-                        size: displayHeight(context) * 0.04,),
-                      content: TextFormField(
-                        controller: productName_controller,
-                        validator:  (productName){
-                          if (productName_controller.text.isEmpty) {
-                            print("validated");
-                            return "* Required";
-                          }
-                          else {
-                            return null;
-                          }
-                        },
-
-                        autofocus: false,
-                        decoration: InputDecoration(
-                          hintText: 'Enter product name',
-                          hintStyle: GoogleFonts.reemKufi(
-                              color: Color(0xffFFFDF4),
-                              fontSize: displayHeight(context) * 0.03),
-                        ),
-                        style: GoogleFonts.reemKufi(
-                            color: Color(0xffFFFDF4),
-                            fontSize: displayHeight(context) * 0.03),
-                      ),
-                      actions: <Widget>[
-                        Padding(
-                          padding:
-                          EdgeInsets.all(displayHeight(context) * 0.03),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              TextButton(
-                                style: TextButton.styleFrom(
-                                    backgroundColor: Color(0xffBBBD88)),
-                                child: ReemKufi_Black(textValue: 'Add',
-                                  size: displayHeight(context) * 0.03,),
-                                onPressed: () async {
-                                  if (_formKey.currentState!.validate()) {
-
-                                    Provider.of<ScannedProductProvider>(context, listen: false).storeScannedProduct(ScannedProduct(
-                                        productName: productName_controller.text,
-                                        ingredientList: ingredientList));
-                                  }
-
-                                },
-                              ),
-                              TextButton(
-                                style: TextButton.styleFrom(
-                                    backgroundColor: Color(0xffBBBD88)),
-                                child: ReemKufi_Black(textValue: 'Cancel',
-                                  size: displayHeight(context) * 0.03,),
-                                onPressed: () {
-                                  Navigator.pushAndRemoveUntil(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (BuildContext context) =>
-                                          MyBottomAppBar(),
-                                    ),
-                                        (route) => false,
-                                  );
-                                },
-                              ),
-                            ],
+                    return InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => IngredientDetails(
+                                ingredient: snapshot.data![index])));
+                      },
+                      child: Container(
+                          alignment: Alignment.center,
+                          margin: EdgeInsets.fromLTRB(8, 8, 8, 8),
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFDADBC6),
+                            borderRadius: BorderRadius.circular(
+                                20), //border corner radius
                           ),
-                        ),
-                      ],
-                    ),
-                 );
-    });
-
-                // Navigator.of(context).push(MaterialPageRoute(
-                //     builder: (context) => CategoriesAndSearch()));
-              },
-              style: ElevatedButton.styleFrom(
-                side: BorderSide(width: displayWidth(context) * 0.2),
-                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                primary: Color(0xFF283618),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25.0),
-                ),
-              ),
-              child: Text('Add to Scanned Products',
-                  style:
-                      GoogleFonts.reemKufi(color: Colors.white, fontSize: 20)),
-            ),
-          ],
-        ),
-      ),
-    );
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                ReemKufi_Green_Italic(
+                                    textValue:
+                                        snapshot.data![index].ingredientName,
+                                    size: displayHeight(context) * 0.02),
+                                IconButton(
+                                    onPressed: () {
+                                      Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                IngredientDetails(
+                                                    ingredient:
+                                                        snapshot.data![index]),
+                                          ));
+                                    },
+                                    icon: Icon(Icons.arrow_forward_rounded)),
+                              ],
+                            ),
+                          )),
+                    );
+                  }),
+            );
+          }
+        });
   }
 }
